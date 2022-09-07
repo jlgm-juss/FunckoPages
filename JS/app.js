e1e4aab5ee9e6b59a185e4cee1234f94be8b6be9
Pereyra Jose <luispereyra14@hotmail.com>
2022-09-06 22:25:20 -0300

    di funcionalidad al btn para agregar y guardar en local storage, faltaria el read del table

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,10 @@
+import Producto from "./classProducto.js";
+
 // Inicializacion de variables
+let listaProductos = JSON.parse(localStorage.getItem("productoGuardadoLS")) ||[];
 const modalFormProductos = new bootstrap.Modal(
-    document.querySelector("#modalForm"));
+  document.querySelector("#modalAgregarProducto")
+);
 const btnAgregarProducto = document.getElementById("btnAgregarProducto");
 let codigo = document.getElementById("codigo");
 let nombre = document.getElementById("nombre");
@@ -9,16 +13,45 @@ let imagen = document.getElementById("imagen");
 let tipo = document.getElementById("tipo");
 let formProducto = document.getElementById("formProducto");
 
-
 // Events
-btnAgregarProducto.addEventListener("click",()=>{mostrarForm(codigo)});
-
-
-
+btnAgregarProducto.addEventListener("click", () => {
+  mostrarForm(codigo);
+});
+formProducto.addEventListener("submit", crearProducto);
 
 // Funciones
 
-function mostrarForm(){
-    modalFormProductos.show();
-    codigo.value = uuidv4();
+function mostrarForm() {
+  modalFormProductos.show();
+  codigo.value = uuidv4();
 }
+
+function crearProducto(e) {
+  e.preventDefault();
+  console.log("vamo a crea un producto");
+  const nuevoProducto = new Producto(
+    codigo.value,
+    nombre.value,
+    descripcion.value,
+    imagen.value,
+    tipo.value
+  );
+  console.log(nuevoProducto);
+  listaProductos.push(nuevoProducto);
+  guardarLS();
+  limpiarForm();
+  console.log(listaProductos);
+  modalFormProductos.hide();
+}
+
+function limpiarForm() {
+    formProducto.reset();
+    nombre.className = "form-control";
+    descripcion.className = "form-control";
+    imagen.className = "form-control";
+    tipo.className = "form-control";
+  }
+
+  function guardarLS(){
+    localStorage.setItem("productoGuardadoLS",JSON.stringify(listaProductos));
+  }
---
+++ JS/classProducto.js
@@ -0,0 +1,9 @@
+export default class Producto {
+  constructor(codigo, nombre, descripcion, imagen, tipo) {
+    this.codigo = codigo;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.imagen = imagen;
+    this.tipo = tipo;
+  }
+}
