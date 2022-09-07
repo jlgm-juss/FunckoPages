86b9763c3c9b52c96d7b11ed58d8964394ae651e
Agostina <lozagostina@gmail.com>
2022-09-07 18:02:36 -0300

    agrego validaciones de login

--- JS/helpers.js
+++ JS/helpers.js
@@ -1,70 +1,81 @@
-export function validarNombre(input){
-    if(input.value.trim().length>=3 && input.value.trim().length<=50 ){
-  
-        input.className='form-control is-valid'
-        return true
-    }else{
-     
-        input.className='form-control is-invalid'
-        return false
-    } 
+export function validarNombre(input) {
+  if (input.value.trim().length >= 3 && input.value.trim().length <= 50) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
 }
-export function validarDescripcion(input){
-    if(input.value.trim().length>=5 && input.value.trim().length<=300 ){
-  
-        input.className='form-control is-valid'
-        return true
-    }else{
-     
-        input.className='form-control is-invalid'
-        return false
-    } 
+export function validarDescripcion(input) {
+  if (input.value.trim().length >= 5 && input.value.trim().length <= 300) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
 }
-export function validarImagen(input){
-    let expReg= /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/
-    if(expReg.test(input.value)){
-
-        input.className='form-control is-valid' 
-        return true 
-    }else{
-   
-        input.className='form-control is-invalid' 
-        return false 
-    }
+export function validarImagen(input) {
+  let expReg = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+  if (expReg.test(input.value)) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
 }
-export function validarGenero(input){
-    if(input.value == 0 || input.value==""){
-        input.className='form-control is-invalid' 
-        return false  
-    } else{
-        input.className='form-control is-valid' 
-        return true   
-    }
+export function validarGenero(input) {
+  if (input.value == 0 || input.value == "") {
+    input.className = "form-control is-invalid";
+    return false;
+  } else {
+    input.className = "form-control is-valid";
+    return true;
+  }
 }
 //precio escrito de esta forma --> $4000 / $500
-export function validarPrecio(input){
-    let expRegPrecio= /\$([0-9])\d*/
-    
-    if (expRegPrecio.test(input.value)){
+export function validarPrecio(input) {
+  let expRegPrecio = /\$([0-9])\d*/;
 
-        input.className='form-control is-valid' 
-        return true 
-    }else{
-   
-        input.className='form-control is-invalid' 
-        return false 
-    }
+  if (expRegPrecio.test(input.value)) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
 }
 
-export function validarStock(input){
-
-    let expRegStock= /^[0-9]+$/;
-    if(expRegStock.test(input.value)){
-        input.className='form-control is-valid' 
-        return true 
-    }else{
-   
-        input.className='form-control is-invalid' 
-        return false 
-    }
+export function validarStock(input) {
+  let expRegStock = /^[0-9]+$/;
+  if (expRegStock.test(input.value)) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
+}
+export function validarEmail(input) {
+  let expRegMail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+  if (expRegMail.test(input.value)) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
+}
+//La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. Sin simbolos
+export function validarContraseña(input) {
+    let expRegContra= /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/
+    if(expRegContra.test(input.value)){
+        input.className = "form-control is-valid";
+        return true;
+      } else {
+        input.className = "form-control is-invalid";
+        return false;
+      }
 }
