d3e26cfca3a85cd1fc90ee56a7757420f6ce6f7b
JLGM <juss.jlgm@gmail.com>
2022-09-07 23:30:23 -0300

    login para usuarios comunes para adaptar funciones

--- JS/login.js
+++ JS/login.js
@@ -1,52 +1,21 @@
-function obtenerListaUsuarios(){
-    let listaUsuarios = JSON.parse(localStorage.getItem('listaUsuariosLs'));
 
-    if(listaUsuarios = null){
-        listaUsuarios=
-        [
-        //Usuario, contraseña
-        ['adminsfunko', 'funk77']
-        ]
-    }
-    return listaUsuarios;
-}
-function validarCredenciales(pUsuario, pContrasena){
-    let listaUsuarios = obtenerListaUsuarios();
-    let bAcceso = false;
-
-    for(let i=0; i < listaUsuarios.length; i++){
-        if(pUsuario == listaUsuarios[i][0] && pContrasena == listaUsuarios[i][1]){
-            bAcceso = true;
-            sessionStorage.setItem('usuarioActivo', listaUsuarios[i][0] + '')
-        }
-    }
-    return bAcceso;
-}
-   
-   
-   
-   
-   
-   
-   
    //PARA CREAR USUARIOS SIN PERMISOS ESPECIALES
-   // const usuario = document.getElementById('usuario')
-    // const pass = document.getElementById('pass')
-    // const button = document.getElementById('button')
+   const usuario = document.getElementById('usuario')
+    const pass = document.getElementById('pass')
+    const button = document.getElementById('button')
 
-    // if(usuario == 'adminFunko' && pass == '743Af'){
-    //     window.location.href = '../page/administrador.html'
-    // }
-
-    // button.addEventListener('click', (e) => {
-    //     e.preventDefault()
-    //     const data = {
-    //         usuario: usuario.value,
-    //         pass: pass.value
+    button.addEventListener('click', (e) => {
+        e.preventDefault()
+        const data = {
+            usuario: usuario.value,
+            pass: pass.value
             
         
-    //     }
+        }
        
-    //     console.log(data)
-    // })
+        console.log(data)
+    })
     
+    // "José... está para agregarle una nueva key para local storage y sumarle el agregar y eliminar
+    // por otro lado hacerle también un .map para crear una tabla con los usuarios comunes
+    // "
