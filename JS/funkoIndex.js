513174a668d7e60422144106417efb4971ba04b5
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-09-08 21:54:04 -0300

    agregue la creacion de productos desde el administrador a index

---
+++ JS/funkoIndex.js
@@ -0,0 +1,17 @@
+let listaProductos = JSON.parse(localStorage.getItem("productoGuardadoLS")) || [];
+
+listaProductos.map((funko)=>{crearCards(funko)});
+
+function crearCards(funko){
+    let cards = document.querySelector("#grillaFunko");
+    cards.innerHTML += `
+    <div class="card col-12 col-md-5 col-lg-2 cardSize">
+    <img src="${funko.imagen}" class="img-fluid" alt="${funko.nombre}">
+    <div class="card-body">
+      <h5 class="card-title">${funko.nombre}</h5>
+      <p class="card-text">${funko.precio}</p>
+        <button type="button" class="btn btn-outline-primary" onclick="detalleFunko('${funko.codigo}')">Detalles</button>
+      <button type="button" class="btn btn-outline-primary">Comprar</button>
+    </div>
+  </div>`
+}
