2006f6b36fb69c543d8fcfd635898a4f4433d3ea
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-09-08 21:32:47 -0300

    agregue las funciones de editar pelicula

--- JS/app.js
+++ JS/app.js
@@ -16,6 +16,7 @@ let tipo = document.getElementById("tipo");
 let precio = document.getElementById("precio");
 let stock = document.getElementById("stock");
 let formProducto = document.getElementById("formProducto");
+let funkoNuevo = true;
 
 // Events
 btnAgregarProducto.addEventListener("click", () => {
@@ -32,13 +33,27 @@ stock.addEventListener('blur', ()=>{validarStock(stock)});
 // Funciones
 
 function mostrarForm() {
+  funkoNuevo = true;
+  limpiarForm();
   modalFormProductos.show();
   codigo.value = uuidv4();
 }
 
-function crearProducto(e) {
+function crearProducto(e){
   e.preventDefault();
+  if(funkoNuevo == true){
+    generarFunko();
+  }else{
+    actuzalizarFunko();
+  }
+}
+
+function generarFunko() {
   console.log("vamo a crea un producto");
+  if(
+    validarNombre(nombre) && validarDescripcion(descripcion) && validarImagen(imagen) && validarGenero(tipo) && validarPrecio(precio) && validarStock(stock)
+  ){
+
   const nuevoProducto = new Producto(
     codigo.value,
     nombre.value,
@@ -52,6 +67,7 @@ function crearProducto(e) {
   listaProductos.push(nuevoProducto);
   guardarLS();
   limpiarForm();
+  crearFila(nuevoProducto);
   console.log(listaProductos);
   Swal.fire(
     "Producto Agregado",
@@ -60,6 +76,9 @@ function crearProducto(e) {
   );
   actualizarTabla();
   modalFormProductos.hide();
+}else{
+  Swal.fire('Ocurrio un error')
+}
 }
 
 function limpiarForm() {
@@ -75,6 +94,7 @@ function limpiarForm() {
 function guardarLS() {
   localStorage.setItem("productoGuardadoLS", JSON.stringify(listaProductos));
 }
+
 cargaInicial();
 function cargaInicial() {
   if (listaProductos.length > 0) {
@@ -108,7 +128,7 @@ ${productos.precio}
 <td>${productos.tipo}</td>
 <td>
 <button class="btn">
-<i class="bi bi-pencil-square text-warning fs-1" ></i>
+<i class="bi bi-pencil-square text-warning fs-1" onclick="editarFunko('${productos.codigo}')" ></i>
 </button>
 <button class="btn">
 <i class="bi bi-clipboard-x-fill text-danger fs-1" onclick="borrarProducto('${productos.codigo}')"></i>
@@ -151,3 +171,38 @@ window.borrarProducto = function (codigo) {
     }
   });
 };
+
+window.editarFunko = function (producto) {
+  funkoNuevo = false;
+
+  modalFormProductos.show();
+
+  let funkoSeleccionado = listaProductos.find((funko) => funko.codigo === producto);
+
+  codigo.value = funkoSeleccionado.codigo;
+  nombre.value = funkoSeleccionado.nombre;
+  descripcion.value = funkoSeleccionado.descripcion;
+  imagen.value = funkoSeleccionado.imagen;
+  tipo.value = funkoSeleccionado.tipo;
+  precio.value = funkoSeleccionado.precio;
+  stock.value = funkoSeleccionado.stock;
+}
+
+function actuzalizarFunko(){
+  let posicionFunko = listaProductos.findIndex((posicion) => posicion.codigo == codigo.value);
+
+  listaProductos[posicionFunko].nombre = nombre.value;
+  listaProductos[posicionFunko].descripcion = descripcion.value;
+  listaProductos[posicionFunko].imagen = imagen.value;
+  listaProductos[posicionFunko].tipo = tipo.value;
+  listaProductos[posicionFunko].precio = precio.value;
+  listaProductos[posicionFunko].stock = stock.value;
+
+  guardarLS();
+
+  actualizarTabla();
+
+  modalFormProductos.hide();
+
+  limpiarForm();
+}
